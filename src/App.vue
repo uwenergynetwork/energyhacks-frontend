9a96e2294a467ed4437fdb0f41b0a0b7e8ce3022
sophie-1984 <sophiet2027@gmail.com>
2019-11-08 19:51:14 -0500

    Fix css to be somewhat responsive

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,15 @@
   </div>
 </template>
 
+<<<<<<< dd3c253f0c0c108505eb0107c8e1f40be46f4862
 <script lang="ts">
 
 import Vue from 'vue';
 import Main from './components/Main.vue'
+=======
+<script>
+import Main from './components/New.vue'
+>>>>>>> Fix css to be somewhat responsive
 
 
 export default Vue.extend({
--- src/components/New.vue
+++ src/components/New.vue
@@ -8,38 +8,35 @@
             <h6 style="color: white;" class="col-12"> Powerful solutions. </h6>
         </div>
         <div class="col-12 pt-5 pb-3 row mx-0">
-            <div style="color: white; font-weight: bold;" class="col-5"> Subscribe to our mailing list to receive important announcements!</div>
+            <div style="color: white; font-weight: bold;" class="col-md-5 col-sm-12"> Subscribe to our mailing list to receive important announcements!</div>
             <div class="w-100 d-none d-md-block"></div>
         </div>
         <div class="col-12 row pb-5 mx-0">
-            <div class="col-4 input-group">
+            <div class="col-sm-12 col-md-4 input-group">
                 <input type="text" class="form-control email" placeholder="Enter your email address" aria-label="Email address">
             </div>
-            <div class="col-2 input-group p-0">
+            <div class="col-sm-12 col-md-2 input-group">
                 <button type="button" class="btn subscribe"><span style="color: white">Subscribe</span></button>
             </div>
         </div>
         <div class="col-12 pt-5 pb-5 row mx-0">
             <h2 style="color: white;" class="col-12"> An energy first hackathon </h2>
-            <div class="col-lg-8 mx-auto p-xl-5 p-lg-4">
                 <div class="row col-12">
-                    <div class="col-xl-6 col-lg-6 col-sm-6 col-6 p-0" style="color: white;">
+                    <div class="col-md-6 col-sm-12" style="color: white;">
                         EnergyHacks is different than your regular, run-of-the-mill hackathon. 
                         This hackathon is focused solely around a series of energy-related problems
                         that exist in our world today, how will you solve them?          
                     </div>
-                    <div class="col-xl-6 col-lg-6 col-sm-6 col-6" style="text-align: center"> 
+                    <div class="col-md-6 col-sm-12" style="text-align: center"> 
                         <img src="http://i.stack.imgur.com/yZlqh.png" style="height:auto; width:50%">
                     </div>
-
-                </div>
             </div>
         </div>
         <div class="col-12 pb-5 row mx-0">
             <h2 style="color: white;" class="col-12">Register</h2>
-             <div class="col-lg-8 pt-2 p-xl-5 p-lg-4">
-                <div class="row col-12">
-                    <div class="col-xl-3 col-lg-4 col-sm-5 col-6 p-2 temp">
+             <div class="col-12 p-0 pt-2">
+                <div class="row col-12 justify-content-between">
+                    <div class="col-sm-12 col-md-5 temp">
                             <div class="col-12 pt-5 pb-4" style="text-align: center"> 
                                 <img src="http://i.stack.imgur.com/yZlqh.png" style="height:auto; width:50%">
                             </div>
@@ -52,7 +49,7 @@
                                 <button type="button" class="btn apply-now">Apply Now</button>
                             </div>                 
                     </div>
-                    <div class="col-xl-3 col-lg-4 col-sm-5 col-6 p-2 mx-auto temp">
+                    <div class="col-sm-12 col-md-5 temp">
                             <div class="col-12 pt-5 pb-4" style="text-align: center"> 
                                 <img src="http://i.stack.imgur.com/yZlqh.png" style="height:auto; width:50%">
                             </div>
@@ -98,7 +95,7 @@ export default Vue.extend({
     border-radius: 20px;
     border-color: black;
     border-width: medium;
-    width: 70%;
+    width: 100%;
 }
 
 .subscribe {
