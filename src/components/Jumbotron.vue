fbf77b5484ce0ea363211664c584917657e15ee9
Jamie Pinheiro <jamiepinheiro@gmail.com>
2020-01-10 23:23:30 -0500

    Jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>EnergyHacks, university of waterlo...</div>
+  <div class="col-md-6 col-sm-12">
+    <h1><b>EnergyHacks</b></h1>
+    <h2 class="pb-1"><b>University of Waterloo · May 23-24, 2020</b></h2>
+    <h5>Sustainability problems.</h5>
+    <h5>Forwarding thinking.</h5>
+    <h5 class="pb-2">Powerful solutions.</h5>
+    <h5 class="pb-5"><b>Applications open early May</b></h5>
+  </div>
 </template>
 
 <script lang='ts'>
@@ -9,6 +16,8 @@ export default Vue.extend({
 });
 </script>
 
-<style>
-
+<style scoped>
+  h {
+    color: #16353A
+  }
 </style>
