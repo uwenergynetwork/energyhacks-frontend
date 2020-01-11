c92e827b754816d4ad59a32986e157e15061e5e9
Jamie Pinheiro <jamiepinheiro@gmail.com>
2020-01-10 23:48:37 -0500

    Mail Signup

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="col-md-6 col-sm-12">
+  <div class='col-md-6 col-sm-12'>
     <h1><b>EnergyHacks</b></h1>
-    <h2 class="pb-1"><b>University of Waterloo · May 23-24, 2020</b></h2>
+    <h2 class='pb-1'><b>University of Waterloo · May 23-24, 2020</b></h2>
     <h5>Sustainability problems.</h5>
     <h5>Forwarding thinking.</h5>
-    <h5 class="pb-2">Powerful solutions.</h5>
-    <h5 class="pb-5"><b>Applications open early May</b></h5>
+    <h5 class='pb-2'>Powerful solutions.</h5>
+    <h5 class='pb-5'><b>Applications open early May</b></h5>
   </div>
 </template>
 
