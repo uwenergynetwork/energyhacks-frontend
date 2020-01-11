8b94b6ec14d6f90833ec2eb895acae375af11ced
Ethan Lee <nathe.eel@gmail.com>
2020-01-11 12:04:25 -0500

    added footer element

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,31 +1,33 @@
 <template>
   <div>
-    <NavBar/>
-    <Jumbotron/>
-    <MailSignup/>
-    <Information/>
-    <HowItWorks/>
-    <Slideshow/>
-    <Faq/>
-    <Sponsorship/>
-    <Footer/>
+    <NavBar />
+    <Jumbotron />
+    <MailSignup />
+    <Information />
+    <HowItWorks />
+    <Slideshow />
+    <Faq />
+    <Sponsorship />
+    <Footer />
   </div>
 </template>
 
 <script lang='ts'>
-import Vue from 'vue';
-import NavBar from './NavBar.vue'
-import Jumbotron from './Jumbotron.vue'
-import MailSignup from './MailSignup.vue'
-import Information from './Information.vue'
-import HowItWorks from './HowItWorks.vue'
-import Slideshow from './Slideshow.vue'
-import Faq from './Faq.vue'
-import Sponsorship from './Sponsorship.vue'
-import Footer from './Footer.vue'
+import Vue from "vue";
+import NavBar from "./NavBar.vue";
+import Jumbotron from "./Jumbotron.vue";
+import MailSignup from "./MailSignup.vue";
+import Information from "./Information.vue";
+import HowItWorks from "./HowItWorks.vue";
+import Slideshow from "./Slideshow.vue";
+import Faq from "./Faq.vue";
+import Sponsorship from "./Sponsorship.vue";
+import Footer from "./Footer.vue";
+
+
 
 export default Vue.extend({
-  name: 'main',
+  name: "main",
   components: {
     NavBar,
     Jumbotron,
@@ -41,4 +43,11 @@ export default Vue.extend({
 </script>
 
 <style>
+body::-webkit-scrollbar {
+  display: none;
+}
+
+body {
+  -ms-overflow-style: none;
+}
 </style>
