b0228af4916c2aa11e9fb508fd1fea05669e7e4c
Jamie Pinheiro <jamiepinheiro@gmail.com>
2020-01-10 23:06:33 -0500

    Register Components

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,21 +1,14 @@
 <template>
-  <div>
-    <Main/>
-  </div>
+  <div>An Energy First Hackathon...</div>
 </template>
 
 <script lang='ts'>
-
 import Vue from 'vue';
-import Main from './components/Main.vue'
 
 export default Vue.extend({
-  name: 'energyhacks',
-  components: {
-    Main
-  }
 });
 </script>
 
 <style>
+
 </style>
