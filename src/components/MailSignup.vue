e2539d18dc37e8dc2080a679d9aa32bca0eab04c
Jamie Pinheiro <jamiepinheiro@gmail.com>
2020-01-12 15:18:48 -0500

    Fix mobile view

--- src/components/MailSignup.vue
+++ src/components/MailSignup.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="col-md-4 col-sm-12 white">
+  <div class="col-md-4 col-sm-12 white mt-">
     <div class="col-md-8 col-sm-12 mb-3">
       <div>
         <b>Subscribe to our mailing list to receive important announcements!</b>
       </div>
     </div>
-    <div class="col-12 row pb-5 mx-0">
+    <div class="col-12 row pb-md-5 mx-0">
       <input
         type="email"
         v-model="email"
-        class="col-md-8 col-sm-12 round white-border py-1 mr-3 white"
+        class="col-md-8 col-sm-12 round white-border mr-md-3 white"
         placeholder="Enter your email address"
       />
       <button
         type="button"
         v-on:click="subscribeEmail"
-        class="col-md-3 col-sm-12 button round white-border"
+        class="col-md-3 col-sm-12 button round white-border mt-md-0 mt-2"
       >
-        <span style="color: white">
+        <span class="white">
           <b>Subscribe</b>
         </span>
       </button>
