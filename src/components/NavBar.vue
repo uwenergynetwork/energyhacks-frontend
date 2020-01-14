7dcae50ecb35bd736246aa8ebdf6146e1ce0daec
Huini Chen <34189742+Harukaichii@users.noreply.github.com>
2020-01-14 15:12:11 +0900

    Navbar Component & Slight CSS Update (#3)
    * fixed css and created NavBar
    
    * added some responsitivity
    
    * add hide on resize function and moved navbar component in overall structure
    
    * moved svg, indented styles and changed to bootstrap for menu hiding

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="navbar">NavBar</div>
+  <div class="navbar">
+    <nav>
+      <a href="#" id="logo">
+        <div id="logo-nav"></div>
+      </a>
+      <div class="right-side d-sm-none d-md-flex">
+        <a href="#">About</a>
+        <a href="#">How it works</a>
+        <a href="#">2019</a>
+        <a href="#">Photos</a>
+        <a href="#">FAQ</a>
+        <a href="#">Sponsors</a>
+        <a href="#">
+          <div class="svg-scale2525" id="fbicon-nav"></div>
+        </a>
+        <a href="#">
+          <div class="svg-scale2525" id="instaicon-nav"></div>
+        </a>
+        <a href="#">
+          <div class="svg-scale2525" id="mailicon-nav"></div>
+        </a>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({
-});
+export default Vue.extend({});
 </script>
 
 <style scoped>
+  #logo-nav{
+    background-image: url("~@/assets/img/website/logo.svg");
+    height:51px;
+    width:51px;
+  }
   .navbar {
     z-index: 1;
-    position: absolute;
-    top: 0; left: 0;
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 80px;
+    padding-top: 0;
+    padding-bottom: 0;
+    display: flex;
+    justify-content: center;
+  }
+  .right-side {
+    margin-left: auto;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    width: 70%;
+  }
+
+  a {
+    color: black;
+    font-weight: bold;
+    font-size: 1em;
+  }
+
+  a:hover {
+    color: black;
+    opacity: 80%;
+    text-decoration: none;
+  }
+  nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+  }
+
+  .svg-scale2525 {
+    height: 25px;
+    width: 25px;
+  }
+
+  #fbicon-nav{
+    background-image: url("~@/assets/img/website/facebook.svg");
+  }
+  
+  #instaicon-nav{
+    background-image: url("~@/assets/img/website/insta.svg");
+  }
+
+  #mailicon-nav{
+    background-image: url("~@/assets/img/website/mail.svg");
   }
 </style>
