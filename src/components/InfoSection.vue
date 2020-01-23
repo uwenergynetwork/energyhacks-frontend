00ec3b3513e4e1c468ea2d36e1f6aa233fd8424a
Huini Chen <34189742+Harukaichii@users.noreply.github.com>
2020-01-24 03:38:33 +0900

    Fixed Navbar resize and added info component (#5)
    * fixed navbar resize and added new component

---
+++ src/components/InfoSection.vue
@@ -0,0 +1,47 @@
+<template>
+	<section :class="(picOnLeft? 'flex-row-reverse row': 'row') ">
+			<div class="col-xs-6 col-sm">
+					<div class="title">{{title}}</div>
+					<div class="desc">{{desc}}</div>
+			</div>
+			<img class="col-sm p-5" :src="getImage(this.pic)">
+	</section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    title: String,
+    desc: String,
+    picOnLeft: Boolean,
+    pic:String
+  },
+  methods:{
+      getImage(loca:String){
+          return require(`@/assets/img/website/${loca}`)
+      }
+  }
+});
+</script>
+
+<style scoped>
+	.title{
+		color:white;
+		font-size: 2em;
+		font-weight: bold;
+		line-height: 124%;
+		margin-bottom: 3%;
+	}
+
+	.desc{
+		color:white;
+		font-size: 1.2em;
+		line-height: 28px;
+	}
+
+	img{
+		box-sizing: border-box;
+	}
+</style>
--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,14 +1,53 @@
 <template>
-  <div>An Energy First Hackathon...</div>
+	<div class="container">
+	<info-section v-for="(content, index) in contents" 
+	:key=content[0] 
+	:title="content.title" 
+	:desc="content.desc" 
+	:picOnLeft="index%2 !== 0"
+	:pic="content.pic" />
+	</div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import InfoSection from "./InfoSection.vue";
 
 export default Vue.extend({
+  components: {
+    InfoSection
+  },
+  data(){
+    return{
+      contents:[
+        { 
+          title: "An Energy First Hackathon", 
+          desc:"Being the only energy focused hackathon in Canada, EnergyHacks offers a unique experience by encouraging participants to come up with solutions for one of the most pressing issues to date.",
+          pic: "infoSect1.svg"
+        },
+        { 
+          title: "Uniting students across all faculties", 
+          desc:"Your solution can be technical or non-technical, which includes apps, proposals for law changes, and more! Get ready for your team to tackle challenges with technical, social, economic, and environmental aspects. We are uniting students to create solutions for a better world!",
+          pic: "infoSect2.svg"
+        },
+        { 
+          title: "We’re sustainable!", 
+          desc:"Being the only energy focused hackathon in Canada, EnergyHacks offers a unique experience by encouraging participants to come up with solutions for one of the most pressing issues to date.",
+          pic: "infoSect3.svg"
+        },
+      ]
+    }
+  }
 });
 </script>
 
-<style>
-
+<style scoped>
+  section{
+    display:flex;
+    height:100%;
+    width: 100%;
+    justify-content: space-around;
+    align-items: center;
+    padding-top:10%;
+  }
 </style>
