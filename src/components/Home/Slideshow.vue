f046b0e621a0b841b235345fb5d7eb8da9b845fe
Ethan Lee <Ethan.Lee@ibm.com>
2020-02-20 14:16:51 -0500

    added trees.css, fixed linting

--- src/components/Home/Slideshow.vue
+++ src/components/Home/Slideshow.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="slideshow-margin">
     <div clas="col-12">
-      <div class="trees tree-1"></div>
-      <div class="trees tree-2"></div>
-      <div class="trees tree-3"></div>
-      <div class="trees tree-4"></div>
-      <div class="trees tree-5"></div>
-      <div class="trees tree-6"></div>
-      <div class="trees tree-7"></div>
+      <div class="trees tree-7 d-none d-md-block"></div>
+      <div class="trees tree-8 d-none d-md-block"></div>
+      <div class="trees tree-9 d-none d-md-block"></div>
+      <div class="trees tree-10 d-none d-md-block"></div>
+      <div class="trees tree-11 d-none d-md-block"></div>
+      <div class="trees tree-12 d-none d-md-block"></div>
       <div class="text-center">
         <h2>
           <b>EnergyHacks 2019</b>
@@ -29,7 +28,10 @@
             :autoplay="true"
             :paginationPosition="bottom"
             :paginationEnabled="true"
-            :perPageCustom="[[320, 1], [1199, 3]]"
+            :perPageCustom="[
+              [320, 1],
+              [1199, 3]
+            ]"
             :loop="true"
             :spacePadding="10"
             :centerMode="true"
@@ -48,128 +50,66 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { Carousel, Slide } from "vue-carousel";
+  import Vue from "vue";
+  import { Carousel, Slide } from "vue-carousel";
 
-export default Vue.extend({
-  data() {
-    return {
-      images: [] as any
-    };
-  },
-  components: {
-    Carousel,
-    Slide
-  },
-  mounted() {
-    this.importAll(
-      require.context("../../assets/img/website/slideshow/", true, /\.png$/)
-    );
-  },
-
-  methods: {
-    importAll(r: any) {
-      r.keys().forEach((key: any) =>
-        this.images.push({ pathLong: r(key), pathShort: key })
+  export default Vue.extend({
+    data() {
+      return {
+        images: [] as any
+      };
+    },
+    components: {
+      Carousel,
+      Slide
+    },
+    mounted() {
+      this.importAll(
+        require.context("../../assets/img/website/slideshow/", true, /\.png$/)
       );
+    },
+
+    methods: {
+      importAll(r: any) {
+        r.keys().forEach((key: any) =>
+          this.images.push({ pathLong: r(key), pathShort: key })
+        );
+      }
     }
-  }
-});
+  });
 </script>
 
 <style scoped>
-.slideshow-margin {
-  margin-bottom: 150px;
-  position: relative;
-}
-#slideshow-bg {
-  box-sizing: border-box;
-  width: 100%;
-  height: 125%;
-  z-index: 0;
-  position: absolute;
-  margin: 20px;
-}
+  @import "../../assets/trees.css";
+  .slideshow-margin {
+    margin-bottom: 150px;
+    position: relative;
+  }
+  #slideshow-bg {
+    box-sizing: border-box;
+    width: 100%;
+    height: 125%;
+    z-index: 0;
+    position: absolute;
+    margin: 20px;
+  }
 
-#slide-style {
-  margin-right: 10px;
-  margin-left: 10px;
-  z-index: 3;
-  position: relative;
-}
+  #slide-style {
+    margin-right: 10px;
+    margin-left: 10px;
+    z-index: 3;
+    position: relative;
+  }
 
-#slideshow-img {
-  width: 100%;
-  height: auto;
-  border-radius: 5px;
-}
+  #slideshow-img {
+    width: 100%;
+    height: auto;
+    border-radius: 5px;
+  }
 
-#carousel {
-  position: relative;
-  z-index: 1;
-  top: 70px;
-}
-
-.trees {
-  position: absolute;
-  z-index: 0;
-  background-position: center bottom;
-  background-repeat: no-repeat;
-  background-size: contain;
-  overflow: hidden;
-}
-
-.tree-1 {
-  background-image: url("~@/assets/img/website/tree-1.svg");
-  position: absolute;
-  top: 107%;
-  right: 85%;
-  width: 40px;
-  height: 40px;
-}
-
-.tree-2 {
-  background-image: url("~@/assets/img/website/tree-2.svg");
-  position: absolute;
-  top: 115%;
-  right: 94%;
-  width: 50px;
-  height: 50px;
-}
-
-.tree-3 {
-  background-image: url("~@/assets/img/website/tree-1.svg");
-  position: absolute;
-  top: 100%;
-  right: 92%;
-  width: 40px;
-  height: 40px;
-}
-
-.tree-4 {
-  background-image: url("~@/assets/img/website/tree-1.svg");
-  position: absolute;
-  top: 117%;
-  right: 1%;
-  width: 70px;
-  height: 70px;
-}
-
-.tree-5 {
-  background-image: url("~@/assets/img/website/tree-1.svg");
-  position: absolute;
-  right: 11%;
-  top: 110%;
-  width: 40px;
-  height: 40px;
-}
-
-.tree-6 {
-  background-image: url("~@/assets/img/website/tree-2.svg");
-  position: absolute;
-  top: 101%;
-  right: 6%;
-  width: 50px;
-  height: 50px;
-}
+  #carousel {
+    position: relative;
+    z-index: 1;
+    top: 70px;
+  }
 </style>
--- src/components/Home/Sponsorship.vue
+++ src/components/Home/Sponsorship.vue
@@ -14,7 +14,10 @@
           target="_blank"
           href="http://td.ca"
         >
-          <img class="col-lg-2 col-3 mx-auto" src="@/assets/img/sponsors/td.svg" />
+          <img
+            class="col-lg-2 col-3 mx-auto"
+            src="@/assets/img/sponsors/td.svg"
+          />
         </a>
       </div>
       <div
@@ -25,21 +28,30 @@
           target="_blank"
           href="http://do.co/studenthackathon"
         >
-          <img class="col-md-10 col-12 mx-auto" src="@/assets/img/sponsors/digital_ocean.svg" />
+          <img
+            class="col-md-10 col-12 mx-auto"
+            src="@/assets/img/sponsors/digital_ocean.svg"
+          />
         </a>
         <a
           class="col-4 mx-auto p-0 p-md-3 d-flex align-items-stretch"
           target="_blank"
           href="https://www.voiceflow.com/"
         >
-          <img class="col-md-10 col-12 mx-auto" src="@/assets/img/sponsors/voiceflow.png" />
+          <img
+            class="col-md-10 col-12 mx-auto"
+            src="@/assets/img/sponsors/voiceflow.png"
+          />
         </a>
         <a
           class="col-4 mx-auto p-0 p-md-3 d-flex align-items-stretch"
           target="_blank"
           href="https://www.github.com/"
         >
-          <img class="col-md-10 col-12 mx-auto" src="@/assets/img/sponsors/github.svg" />
+          <img
+            class="col-md-10 col-12 mx-auto"
+            src="@/assets/img/sponsors/github.svg"
+          />
         </a>
       </div>
     </div>
@@ -49,49 +61,71 @@
     <div
       class="row col-lg-8 col-md-11 col-12 mx-auto d-flex align-items-stretch justify-content-around"
     >
-      <div class="col-sm-12 col-md-4 px-3 d-flex align-items-stretch mb-1 my-md-0">
+      <div
+        class="col-sm-12 col-md-4 px-3 d-flex align-items-stretch mb-1 my-md-0"
+      >
         <div class="col-12 box px-4 pb-4 box-1 shadow-md">
           <img class="logo" src="@/assets/img/website/network-icon.svg" />
           <h5 class="my-3">
             <b>Networking Opportunities</b>
           </h5>
-          <p>Sponsors are welcome to network with students throughout the event. With coop’s prevalence at the University of Waterloo, the vast majority of attendees will be interested in hearing about your company.</p>
+          <p>
+            Sponsors are welcome to network with students throughout the event.
+            With coop’s prevalence at the University of Waterloo, the vast
+            majority of attendees will be interested in hearing about your
+            company.
+          </p>
         </div>
       </div>
 
-      <div class="col-sm-12 col-md-4 px-3 d-flex align-items-stretch mb-1 my-md-0">
+      <div
+        class="col-sm-12 col-md-4 px-3 d-flex align-items-stretch mb-1 my-md-0"
+      >
         <div class="col-12 box px-4 pb-4 box-2 shadow-md">
           <img class="logo" src="@/assets/img/website/brand-awareness.svg" />
           <h5 class="my-3">
             <b>Brand Awareness</b>
           </h5>
-          <p>Last year, our social media campaigns reached over 20,000 individuals and our website had over 12,000 unique pageviews. As a sponsor, your brand image will be further tied towards this initiative at scale, and will help develop positive associations with your company among the student body.</p>
+          <p>
+            Last year, our social media campaigns reached over 20,000
+            individuals and our website had over 12,000 unique pageviews. As a
+            sponsor, your brand image will be further tied towards this
+            initiative at scale, and will help develop positive associations
+            with your company among the student body.
+          </p>
         </div>
       </div>
 
-      <div class="col-sm-12 col-md-4 px-3 d-flex align-items-stretch mb-1 my-md-0">
+      <div
+        class="col-sm-12 col-md-4 px-3 d-flex align-items-stretch mb-1 my-md-0"
+      >
         <div class="col-12 box px-4 pb-4 box-3 shadow-md">
           <img class="logo" src="@/assets/img/website/marketing.svg" />
           <h5 class="my-3">
             <b>Marketing</b>
           </h5>
-          <p>As a sponsor, your company brand will be at the center of this joint endeavor. Your branding will be present on any apparel present at the event and center stage on the hackathon’s website along with any promotional material.</p>
+          <p>
+            As a sponsor, your company brand will be at the center of this joint
+            endeavor. Your branding will be present on any apparel present at
+            the event and center stage on the hackathon’s website along with any
+            promotional material.
+          </p>
         </div>
       </div>
-      <div class="trees tree-1"></div>
-      <div class="trees tree-2"></div>
-      <div class="trees tree-3"></div>
-      <div class="trees tree-4"></div>
-      <div class="trees tree-5"></div>
-      <div class="trees tree-6"></div>
-      <div class="trees tree-7"></div>
-      <div class="trees tree-8"></div>
-      <div class="trees tree-9"></div>
+      <div class="trees tree-13 d-none d-md-block"></div>
+      <div class="trees tree-14 d-none d-md-block"></div>
+      <div class="trees tree-15 d-none d-md-block"></div>
+      <div class="trees tree-16 d-none d-md-block"></div>
+      <div class="trees tree-17 d-none d-md-block"></div>
+      <div class="trees tree-18 d-none d-md-block"></div>
     </div>
 
-    <p
-      class="col-sm-10 col-md-8 col-lg-6 mx-auto mt-5 white"
-    >Energy Hacks 2020 will bring forth innovative solutions and ideas to the energy industry. You can help invest in the future of energy by contacting us for more details on sponsorship opportunities and benefits available to you.</p>
+    <p class="col-sm-10 col-md-8 col-lg-6 mx-auto mt-5 white">
+      Energy Hacks 2020 will bring forth innovative solutions and ideas to the
+      energy industry. You can help invest in the future of energy by contacting
+      us for more details on sponsorship opportunities and benefits available to
+      you.
+    </p>
 
     <button
       v-on:click="sponsorship"
@@ -103,104 +137,42 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+  import Vue from "vue";
 
-export default Vue.extend({
-  methods: {
-    sponsorship() {
-      window.open("mailto:energyhacks@uwen.ca");
+  export default Vue.extend({
+    methods: {
+      sponsorship() {
+        window.open("mailto:energyhacks@uwen.ca");
+      }
     }
-  }
-});
+  });
 </script>
 
 <style scoped>
-.box {
-  margin-top: 20%;
-  background-color: #f4ffff;
-  border-radius: 0.5vw;
-  padding-top: 15%;
-  color: #16353a;
-}
+  @import "../../assets/trees.css";
+  .box {
+    margin-top: 20%;
+    background-color: #f4ffff;
+    border-radius: 0.5vw;
+    padding-top: 15%;
+    color: #16353a;
+  }
 
-.box-2 {
-  color: #547c83;
-}
+  .box-2 {
+    color: #547c83;
+  }
 
-.box-3 {
-  color: #88a550;
-}
+  .box-3 {
+    color: #88a550;
+  }
 
-.logo {
-  width: 100px;
-  opacity: 1;
-  position: absolute;
-  margin: auto;
-  top: -50px;
-  right: 0;
-  left: 0;
-}
-
-.trees {
-  position: absolute;
-  z-index: 0;
-  background-position: center bottom;
-  background-repeat: no-repeat;
-  background-size: contain;
-  overflow: hidden;
-}
-
-.tree-1 {
-  background-image: url("~@/assets/img/website/tree-blue-1.svg");
-  position: absolute;
-  top: 102%;
-  right: 103%;
-  width: 40px;
-  height: 40px;
-}
-
-.tree-2 {
-  background-image: url("~@/assets/img/website/tree-blue-2.svg");
-  position: absolute;
-  top: 105%;
-  right: 118%;
-  width: 50px;
-  height: 50px;
-}
-
-.tree-3 {
-  background-image: url("~@/assets/img/website/tree-blue-1.svg");
-  position: absolute;
-  top: 93%;
-  right: 114%;
-  width: 40px;
-  height: 40px;
-}
-
-.tree-4 {
-  background-image: url("~@/assets/img/website/tree-blue-1.svg");
-  position: absolute;
-  top: 107%;
-  right: -22%;
-  width: 70px;
-  height: 70px;
-}
-
-.tree-5 {
-  background-image: url("~@/assets/img/website/tree-blue-1.svg");
-  position: absolute;
-  right: -9%;
-  top: 101%;
-  width: 40px;
-  height: 40px;
-}
-
-.tree-6 {
-  background-image: url("~@/assets/img/website/tree-blue-2.svg");
-  position: absolute;
-  top: 92%;
-  right: -19%;
-  width: 50px;
-  height: 50px;
-}
+  .logo {
+    width: 100px;
+    opacity: 1;
+    position: absolute;
+    margin: auto;
+    top: -50px;
+    right: 0;
+    left: 0;
+  }
 </style>
