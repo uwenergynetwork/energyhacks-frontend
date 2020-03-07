02966d53110edf03ac55d1827a9529b762cd663f
Jamie Pinheiro <jamiepinheiro@gmail.com>
2020-03-07 16:40:01 -0500

    Begin application sidebar

--- src/components/Apply/Apply.vue
+++ src/components/Apply/Apply.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="col-12">
-    Apply!
+  <div class="col-12 p-0 m-0 row">
+    <SideBar/>
+    <div class="d-flex col-6">Apply</div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+  import Vue from "vue";
+  import SideBar from "./SideBar.vue"
 
-export default Vue.extend({
-  components: {
-    
-  }
-});
+  export default Vue.extend({
+    components: {
+      SideBar
+    }
+  });
 </script>
 
 <style scoped>
---
+++ src/components/Apply/SideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex col-12 col-lg-3 col-xl-2 p-0">
+    <div id="sidebar-big" class="d-none d-lg-block col-12 text-center">
+      <img id="logo" src="@/assets/img/application/logo.svg" class="mt-5"/>
+    </div>
+    <div class="d-lg-none col-12">
+      SideBar Small
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    completed: Array
+  }
+});
+</script>
+
+<style scoped>
+#sidebar-big {
+  height: 100vh;
+  background-color: #16353A;
+}
+#logo {
+  height: 50px;
+  width: 50px;
+}
+</style>
