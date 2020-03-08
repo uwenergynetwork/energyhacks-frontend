e27d17c67b6ede058fa417ec5987593c06ea9081
Harukaichii <h445chen@edu.uwaterloo.ca>
2020-03-08 22:00:57 +0900

    finished large screen sidebar

--- src/components/Apply/SideBar.vue
+++ src/components/Apply/SideBar.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="d-flex col-12 col-lg-3 col-xl-2 p-0">
     <div id="sidebar-big" class="d-none d-lg-block col-12 text-center">
-      <img id="logo" src="@/assets/img/application/logo.svg" class="mt-5"/>
+      <img id="logo" src="@/assets/img/application/logo.svg" class="my-5"/>
+      <div>
+        <side-bar-nav-button  
+          v-for="step in steps"
+          :key="step[0]"
+          :name="step.name"
+          :completed="step.completed"
+          ></side-bar-nav-button>
+      </div>
     </div>
     <div class="d-lg-none col-12">
       SideBar Small
@@ -11,10 +19,23 @@
 
 <script lang="ts">
 import Vue from "vue";
+import SideBarNavButton from "./SideBarNavButton.vue";
 
 export default Vue.extend({
   props: {
     completed: Array
+  },
+  components:{
+    SideBarNavButton
+  },
+  data(){
+    return {
+      steps:[
+        {name:"Personal", completed:false},
+        {name:"Hackathon", completed:false},
+        {name:"Questions", completed:false},
+        {name:"Logistics", completed:false}
+      ]}
   }
 });
 </script>
---
+++ src/components/Apply/SideBarNavButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <a class="row p-2">
+    <b class="col-2">{{name}}</b> 
+    <div class="col-10 status">{{completed? "Completed" : "Not Completed"}}</div>
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    name: String,
+    completed: Boolean
+  }
+});
+</script>
+
+<style scoped>
+
+a > *{
+  color:white;
+}
+
+b{
+  font-size:0.9em;
+}
+
+.status{
+  text-align:right;
+  font-style:italic;
+  font-weight:normal;
+  font-size:0.75em;
+}
+
+/* .example-content-secondary {
+  @include make-col-ready();
+
+  @include media-breakpoint-up(sm) {
+    @include make-col(6);
+  }
+  @include media-breakpoint-up(lg) {
+    @include make-col(4);
+  }
+} */
+
+</style>
